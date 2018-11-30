2ae7a35a122425be73bfea0e0c314c117ffdcbb8
Juan Luis Almazo <jluisalmazo@hotmail.com>
2018-11-30 02:46:58 -0600

    Changed enviroment.prod.ts variables. It is a test for deployment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,11 @@
-
-import apikeys from './firebase-config';
-
 export const environment = {
   production: true,
   firebase: {
-    apiKey: apikeys.firebase_apiKey,
-    authDomain: apikeys.firebase_authDomain,
-    databaseURL: apikeys.firebase_databaseURL,
-    projectId: apikeys.firebase_projectId,
-    storageBucket: apikeys.firebase_storageBucket,
-    messagingSenderId: apikeys.firebase_messagingSenderId
+    apiKey: firebase_apiKey,
+    authDomain: firebase_authDomain,
+    databaseURL: firebase_databaseURL,
+    projectId: firebase_projectId,
+    storageBucket: firebase_storageBucket,
+    messagingSenderId: firebase_messagingSenderId
   }
 };
