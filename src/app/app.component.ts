b639dd9a7ae53e33fc40f48be9e999b341bc85bf
Juan Luis Almazo <jluisalmazo@hotmail.com>
2018-11-30 11:57:28 -0600

    Code was cleaned.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent{
   items$: Observable<any[]>;
 
   constructor(db: AngularFireDatabase){
+
     this.items$ = db.list('questions').valueChanges();
 
     this.items$.subscribe(items => {
@@ -24,10 +25,13 @@ export class AppComponent{
       let index = 1;
         
       items.map(item => {
-        let itemRights = item.rights != undefined ? item.rights : 0;
-        let itemWrongs = item.wrongs != undefined ? item.wrongs : 0;
+        // let itemRights = item.rights != undefined ? item.rights : 0;
+        // let itemWrongs = item.wrongs != undefined ? item.wrongs : 0;
 
-        questions.push("pregunta-" + index);
+        let itemRights = item.rights;
+        let itemWrongs = item.wrongs;
+
+        questions.push("pregunta-" + index + " ");
         rights.push(itemRights);
         wrongs.push(itemWrongs);
         
@@ -52,91 +56,11 @@ export class AppComponent{
         marker: {color: 'orange'}
       };
       
-      let data = [trace1, trace2];
-      
-      console.log(data);
-  
+      let data = [trace1, trace2];        
       let layout = {title: 'Cantidad de aciertos y errores por pregunta', barmode: 'stack', xaxis: {title: 'Aciertos - Errores'}};
   
       Plotly.newPlot('chart', data, layout, {responsive: true});
-    });
+    }); 
 
   }
-
-
-  /*
-
-  ngOnInit() {
-
-    // let trace1 = {
-    //   x: [20, 14, 23],
-    //   y: ['giraffes', 'orangutans', 'monkeys'],
-    //   name: 'SF Zoo',
-    //   type: 'bar',
-    //   orientation: 'h',
-    //   marker: {color: 'magenta'}
-    // };
-    
-    // let trace2 = {
-    //   x: [12, 18, 29],
-    //   y: ['giraffes', 'orangutans', 'monkeys'],
-    //   name: 'LA Zoo',
-    //   type: 'bar',
-    //   orientation: 'h',
-    //   marker: {color: 'blue'}
-    // };
-    // var data = [trace1, trace2];
- 
-
-    
-
-
-    
-    this.items$.subscribe(items => {
-
-      let questions = [];
-      let rights = [];
-      let wrongs = [];
-      let index = 0;
-        
-      items.map(item => {
-        let itemRights = item.rights != undefined ? item.rights : 0;
-        let itemWrongs = item.wrongs != undefined ? item.wrongs : 0;
-
-        questions.push("pregunta-" + index);
-        rights.push(itemRights);
-        wrongs.push(itemWrongs);
-        
-        index++;
-      })
-
-      let trace1 = {
-        y: questions,
-        x: rights,
-        name: 'Rights',
-        type: 'bar',
-        orientation: 'h',
-        marker: {color: 'magenta'}
-      };
-      
-      let trace2 = {
-        y: questions,
-        x: wrongs,
-        name: 'Wrongs',
-        type: 'bar',
-        orientation: 'h',
-        marker: {color: 'blue'}
-      };
-      
-      let data = [trace1, trace2];
-      
-      console.log(data);
-  
-      let layout = {barmode: 'stack'};
-  
-      Plotly.newPlot('chart', data, layout);
-    });
-
-  } */ 
-
 }
