f6418e5e513809cb0713f11ccbe9243844ae61c5
Juan Luis Almazo <jluisalmazo@hotmail.com>
2018-11-30 02:14:08 -0600

    Firebase and Plotly were included.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,18 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import apikeys from './firebase-config';
+  
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: apikeys.firebase_apiKey,
+    authDomain: apikeys.firebase_authDomain,
+    databaseURL: apikeys.firebase_databaseURL,
+    projectId: apikeys.firebase_projectId,
+    storageBucket: apikeys.firebase_storageBucket,
+    messagingSenderId: apikeys.firebase_messagingSenderId
+  }
 };
 
 /*
