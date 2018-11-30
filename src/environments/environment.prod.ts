a211d7f85d1961b4bd4f25ba5137df3899c1afe6
Juan Luis Almazo <jluisalmazo@hotmail.com>
2018-11-30 03:42:00 -0600

    One more deployment test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  firebase: process.env.firebase
+  firebase: {
+    apiKey: {firebase_apiKey},
+    authDomain: {firebase_authDomain},
+    databaseURL: {firebase_databaseURL,
+    projectId: {firebase_projectId},
+    storageBucket: {firebase_storageBucket},
+    messagingSenderId: {firebase_messagingSenderId}
+  }
 };
