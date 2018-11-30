a21529b967a8e78724278e4e8be073b9393c5dfb
Juan Luis Almazo <jluisalmazo@hotmail.com>
2018-11-30 03:02:13 -0600

    deployment test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: firebase_apiKey,
-    authDomain: firebase_authDomain,
-    databaseURL: firebase_databaseURL,
-    projectId: firebase_projectId,
-    storageBucket: firebase_storageBucket,
-    messagingSenderId: firebase_messagingSenderId
-  }
 };
